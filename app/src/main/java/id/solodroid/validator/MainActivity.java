e01c7b76854991558d1a114f5fe693275bc037ea
solodroiddev <dimasword@gmail.com>
2019-12-13 11:27:41 +0700

    first commit

---
+++ app/src/main/java/id/solodroid/validator/MainActivity.java
@@ -0,0 +1,14 @@
+package id.solodroid.validator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
